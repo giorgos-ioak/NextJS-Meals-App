062b5e91af4e8c1e2cc26cb7ff2a78cb4f19ed17
Giorgos <giorgosmintis@gmail.com>
2025-02-21 14:28:11 +0200

    Added more features

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,17 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-function MealsPage() {
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals}/>
+}
+
+async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +29,9 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   )
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+
+
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Meal not found</h1>
+      <p>Unfortunately we could not find the requested page or resource.</p>
+    </main>
+  )
+}
+
+export default NotFound
